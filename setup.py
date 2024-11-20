4e707691ef1ac334571246a631cfea7908aa9159
ayaz-125 <ayazr425@gmail.com>
2024-11-20 13:02:43 +0530

    setup.py file ready

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+The setup.py file is an essential part of packaging
+and distributing Python projects. It is used by setuptools
+(or distutils in older Python versions) to define the configuration
+of your project, such as its metadata, dependencies, and more.
+"""
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """
+    This function retrieves all the requirements for the project
+    by reading them from the requirements.txt file.
+    """
+    requirement_list: List[str] = []
+    try:
+        with open("requirements.txt", "r") as file:
+            # Read lines from the file
+            lines = file.readlines()
+            # Process each line
+            for line in lines:
+                requirement = line.strip()
+                # Ignore empty lines and '-e .'
+                if requirement and requirement!= '-e .':
+                    requirement_list.append(requirement)
+
+    except FileNotFoundError:
+        print("requirements.txt file is not found. Please ensure it exists.")
+
+    return requirement_list
+
+# Print requirements for debugging purposes
+print("Requirements:", get_requirements())
+
+# Define the setup configuration
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Ayaz Rabbani",
+    author_email="ayazr425@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
